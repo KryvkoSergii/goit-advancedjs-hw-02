aedf55566a5eecdfee241c15af688984f3ed6659
Sergii <kryvkosergii@gmail.com>
2024-08-04 01:30:35 +0300

    HW2

---
+++ src/js/1-timer.js
@@ -0,0 +1,83 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+let selectedDate;
+const startBtn = document.querySelector("button");
+const selector = document.querySelector("input#datetime-picker");
+
+const daysFiled = document.querySelectorAll('[data-days]');
+const hoursFiled = document.querySelectorAll('[data-hours]');
+const minutesFiled = document.querySelectorAll('[data-minutes]');
+const secondField = document.querySelectorAll('[data-seconds]');
+let timerId;
+
+function convertMs(ms) {
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    // Remaining days
+    const days = Math.floor(ms / day);
+    // Remaining hours
+    const hours = Math.floor((ms % day) / hour);
+    // Remaining minutes
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    // Remaining seconds
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+    return { days, hours, minutes, seconds };
+}
+
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+        selectedDate = selectedDates[0];
+
+        if (selectedDate.getTime() - Date.now() < 0) {
+            iziToast.show({
+                title: 'Error',
+                message: "Please choose a date in the future",
+                position: "topRight",
+                backgroundColor: 'rgb(255,0,0)'
+            });
+            startBtn.disabled = true;
+        } else {
+            startBtn.disabled = false;
+        }
+    },
+};
+
+flatpickr(selector, options);
+
+function formatNumber(value) {
+    const val = value.toString();
+    return val.length === 1 ? "0" + val : val;
+}
+
+function renderNumbers() {
+    const differnce = selectedDate.getTime() - Date.now();
+    if (differnce > 0) {
+        //render
+        const calculated = convertMs(differnce);
+
+        daysFiled[0].innerText = formatNumber(calculated['days']);
+        hoursFiled[0].innerText = formatNumber(calculated['hours']);
+        minutesFiled[0].innerText = formatNumber(calculated['minutes']);
+        secondField[0].innerText = formatNumber(calculated['seconds']);
+    } else {
+        clearInterval(timerId);
+        startBtn.disabled = false;
+    }
+}
+
+startBtn.addEventListener("click", () => {
+    startBtn.disabled = true;
+    timerId = setInterval(renderNumbers, 1000);
+});
---
+++ src/js/2-snackbar.js
@@ -0,0 +1,54 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const mainForm = document.querySelectorAll("form")[0];
+const btn = document.querySelectorAll("button")[0];
+
+function makeTimer(delay, selectedRadioBtnValue) {
+    console.log(delay + selectedRadioBtnValue);
+    new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (selectedRadioBtnValue === "fulfilled") {
+                resolve(`✅ Fulfilled promise in ${delay}ms`);
+            } else {
+                reject(`❌ Rejected promise in ${delay}ms`);
+            }
+        }, delay);
+    })
+        .then(value => {
+            iziToast.show({
+                message: value,
+                position: "topRight",
+                backgroundColor: 'rgb(0,128,0)',
+                messageColor: 'rgb(255,255,255)'
+            });
+        })
+        .catch(error => {
+            iziToast.show({
+                message: error,
+                position: "topRight",
+                backgroundColor: 'rgb(250,128,114)',
+                messageColor: 'rgb(255,255,255)'
+            });
+        });
+}
+
+const delay = localStorage.getItem("delay");
+const selectedRadioBtnValue = localStorage.getItem("selectedRadioBtnValue");
+
+if (delay && selectedRadioBtnValue) {
+    makeTimer(delay, selectedRadioBtnValue);
+
+    localStorage.removeItem("delay");
+    localStorage.removeItem("selectedRadioBtnValue");
+}
+
+
+btn.addEventListener("click", (event) => {
+    const delay = mainForm.elements['delay'].value;
+    //get selected radio button
+    const selectedRadioBtnValue = mainForm.elements['state'].value;
+
+    localStorage.setItem("delay", delay);
+    localStorage.setItem("selectedRadioBtnValue", selectedRadioBtnValue);
+});
