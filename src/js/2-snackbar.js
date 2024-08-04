7096514d66a487a0eca3164818366241c1f976f6
Sergii <kryvkosergii@gmail.com>
2024-08-04 18:26:16 +0300

    fix

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,11 +1,11 @@
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
-const mainForm = document.querySelectorAll(".snackbar-section form")[0];
-const btn = document.querySelectorAll(".snackbar-section button")[0];
+const mainForm = document.querySelectorAll("form")[0];
+const btn = document.querySelectorAll("button")[0];
 
 function makeTimer(delay, selectedRadioBtnValue) {
-    console.log(delay + selectedRadioBtnValue);
+    
     new Promise((resolve, reject) => {
         setTimeout(() => {
             if (selectedRadioBtnValue === "fulfilled") {
